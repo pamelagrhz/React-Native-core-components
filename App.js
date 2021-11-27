937327f5dfdbf1df8735f06ce54149d76a509fdd
pamelagrhz <pamelagrhz@gmail.com>
2021-11-26 18:49:28 -0600

    Expo installed

---
+++ App.js
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import NavigationStack from './src/navigation/NavigationStack';
+import NavigationTab from './src/navigation/NavigationTab';
+import NavigationDrawer from './src/navigation/NavigationDrawer';
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {/* <NavigationStack/> */}
+      {/* <NavigationTab></NavigationTab> */}
+      <NavigationDrawer></NavigationDrawer>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
